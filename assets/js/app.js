8016eb45f842d1da507fb0385ef678d384a68933
tanvipatil2102 <tanvi6829@gmail.com>
2024-01-09 11:24:44 +0530

    work done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,22 @@
 let cl = console.log;
 
 let navbar = document.getElementById('navBar');
+let changecolor = [...document.querySelectorAll(".changeColor")];
+
 
 const onscroll = () => { 
     let scrollValue = window.scrollY;
     cl(scrollValue);
    if(scrollValue < 120){
-    navbar.classList.remove("bg-light")
+    navbar.classList.remove("bg-light");
+    changecolor.forEach(ele=>{
+        ele.classList.remove("color-primary")
+    })
    }else{
-    navbar.classList.add("bg-light")
+    navbar.classList.add("bg-light");
+    changecolor.forEach(ele=>{
+        ele.classList.add("color-primary")
+    })
    }
 };
 
